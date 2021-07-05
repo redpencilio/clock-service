dba4c5ff2153366d7ea9796c9ba5a6c03ef770a0
Jan-Pieter Baert <jan-pieter@baert.jp.net>
2021-07-05 21:44:35 +0200

    Add implementation with semantic web

--- app.js
+++ app.js
@@ -1,5 +1,44 @@
-import { app, uuid } from "mu";
+import {
+    app,
+    uuid,
+    query,
+    sparqlEscape
+} from "mu";
 
-app.get("/clock", function (req, res) {
-  res.send(JSON.stringify({ time: 0 }));
-});
+app.post("/clock/update/:id", function(req, res) {
+    let id = req.params.id;
+    let uuidValue = uuid();
+    let now = new Date();
+    let value = sparqlEscape(JSON.stringify({
+        time: now.toLocaleDateString(undefined, {
+            timeZone: "Europe/Brussels",
+            dateStyle: 'full',
+            timeStyle: 'long',
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        }),
+    }), 'string')
+    let type = "http://clock"
+    let q = `
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+    PREFIX mupush: <http://mu.semte.ch/vocabularies/push/>
+    INSERT INTO <http://mu.semte.ch/application> {
+        <http://semte.baert.jp.net/push-updates/v0.1/${uuidValue}>  mu:uuid ${sparqlEscape(uuidValue, 'string')};
+                                                                    a mupush:PushUpdate;
+                                                                    mupush:tabId ${sparqlEscape(id, 'string')};
+                                                                    mupush:type <${type}>;
+                                                                    rdf:value ${value}.
+    }
+    `
+    query(q)
+        .then(() => {
+            console.log(`Added push update for ${id}`)
+        })
+        .catch((err) => {
+            console.error(err)
+        })
+    res.status(204).send()
+})
